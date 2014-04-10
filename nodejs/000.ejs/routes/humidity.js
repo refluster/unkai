c9faea85b96cadc6f4f4d319fe35205b73def707
refluster <refluster>
2014-04-11 07:39:17 +0900

    IssueID #16 : bugfix of humidity.js in the case of lack of data from humidity sensor

--- nodejs/000.ejs/routes/humidity.js
+++ nodejs/000.ejs/routes/humidity.js
@@ -19,12 +19,12 @@ exports.init_socket = function(io, client){
 
     // for humidity
     client.on('humidity get_value', function(data) {
+		var humidity, celsius, fahrenheit;
 		console.log("get humidity");
 		
 		// get humidity ,celsius, fahrenheit
 		var cmd = "../../c/002.humidity-sensor/002.humidity -1";
 		exec(cmd, {timeout: 1000}, function(error, stdout, stderr) {
-			var humidity, celsius, fahrenheit;
 			console.log('stdout: '+(stdout||'none'));
 			console.log('stderr: '+(stderr||'none'));
 			if (error !== null) {
@@ -32,10 +32,11 @@ exports.init_socket = function(io, client){
 			} else {
 				var re = /humidity:(\d+\.\d+) celsius:(\d+\.\d+) fahrenheit:(\d+\.\d+)/;
 				var re_match = stdout.match(re);
-				humidity = re_match[1];
-				celsius = re_match[2];
-				fahrenheit = re_match[3];
-
+				if (re_match) {
+					humidity = re_match[1];
+					celsius = re_match[2];
+					fahrenheit = re_match[3];
+				}
 			}
 			client.emit('humidity ret_value', { humidity: humidity,
 												celsius: celsius,
