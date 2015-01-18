856c7d6c8deb7f02ec86be5895ec1e8e5ed4f3f0
refluster <refluster@gmail.com>
2015-01-18 15:28:25 +0900

    IssueID #56 : improve njl5702 driver to get median value

--- c/007.njl7502/main.c
+++ c/007.njl7502/main.c
@@ -1,20 +1,41 @@
 #include <wiringPiSPI.h>
 #include <stdio.h>
+#include <sys/time.h>
+
+#define SAMPLE_COUNT 3
+
+float med3(float *f) {
+    if (f[0] < f[1])
+        if (f[1] < f[2]) return f[1]; else if (f[2] < f[0]) return f[0]; else return f[2];
+	else
+        if (f[2] < f[1]) return f[1]; else if (f[0] < f[2]) return f[0]; else return f[2];
+}
 
 int main() {
-	unsigned char data[] = {0b00000110, 0b00000000, 0b00000000};
-	float lux;
+	float lux[SAMPLE_COUNT];
+	int i;
+	float median;
 
 	// setup wiringpi spi
-	wiringPiSPISetup (0, 1000000) ;
+	wiringPiSPISetup(0, 1000000) ;
 
-	// send and receive data with spi
-	wiringPiSPIDataRW (0, data, sizeof(data));
-	
-	// convert unit to lux
-	lux = (float)(((data[1] << 8) + data[2]) & 0x0FFF) * 1200.0f / 4096.0f;
-	
-	printf("-- %.2f\n", lux);
+	for (i = SAMPLE_COUNT - 1; i >= 0; --i) {
+		unsigned char data[] = {0b00000110, 0b00000000, 0b00000000};
 
+		// send and receive data with spi
+		wiringPiSPIDataRW (0, data, sizeof(data));
+	
+		// convert unit to lux
+		lux[i] = (float)(((data[1] << 8) + data[2]) & 0x0FFF) * 1200.0f / 4096.0f;
+	}
+
+#if SAMPLE_COUNT == 3
+	median = med3(lux);
+#else
+# error "implement a function to find the median"
+#endif	
+
+	printf("illuminance: %.2f lux\n", median);
+	
 	return 0;
 }
