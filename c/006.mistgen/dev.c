edbf472a80ac87d71df2b2afa7e5c1f79851ff3f
refluster <refluster@gmail.com>
2015-01-18 18:15:17 +0900

    IssueID #56 : mistgen change gpio pin number

--- c/006.mistgen/dev.c
+++ c/006.mistgen/dev.c
@@ -1,17 +1,17 @@
 #include <wiringPi.h>
 
-#define MIST_PIN 7
+#define MIST_DATA_PIN 3
 
 void mist_on() {
-	digitalWrite(MIST_PIN, HIGH);
+	digitalWrite(MIST_DATA_PIN, HIGH);
 }
 
 void mist_off() {
-	digitalWrite(MIST_PIN, LOW);
+	digitalWrite(MIST_DATA_PIN, LOW);
 }
 
 int mistgen_init() {
 	wiringPiSetup();
-	pinMode(MIST_PIN, OUTPUT);
+	pinMode(MIST_DATA_PIN, OUTPUT);
 	return 0;
 }
