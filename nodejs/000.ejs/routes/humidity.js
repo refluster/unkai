7971ed71450cad02054e493f24397d5f5e1c2c12
refluster <refluster@gmail.com>
2015-01-19 07:44:16 +0900

    IssueID #56 : refactoring humidity sensor web interface

--- nodejs/000.ejs/routes/humidity.js
+++ nodejs/000.ejs/routes/humidity.js
@@ -35,35 +35,20 @@ exports.index = function(req, res){
 	}
 	console.log('UA ' + ua);
 	console.log('getting humidity from ' + req.headers.host);
-	res.render('humidity', { title: 'humidity and temperature', host: req.headers.host, is_mobile: is_mobile});
+	res.render('humidity', { title: 'humidity and temperature',
+							 host: req.headers.host, is_mobile: is_mobile});
 };
 
 exports.init_socket = function(io, client){
-
 	// for humidity
 	client.on('humidity get_value', function(data) {
 		var humidity, celsius, fahrenheit;
 		console.log("get humidity");
 
-		// get humidity ,celsius, fahrenheit
-		var cmd = "../../c/002.humidity-sensor/002.humidity -1";
-		exec(cmd, {timeout: 1000}, function(error, stdout, stderr) {
-			console.log('stdout: '+(stdout||'none'));
-			console.log('stderr: '+(stderr||'none'));
-			if (error !== null) {
-				console.log('exec error: '+error);
-			} else {
-				var re = /humidity:(\d+\.\d+) celsius:(\d+\.\d+) fahrenheit:(\d+\.\d+)/;
-				var re_match = stdout.match(re);
-				if (re_match) {
-					humidity = re_match[1];
-					celsius = re_match[2];
-					fahrenheit = re_match[3];
-				}
-			}
-			client.emit('humidity ret_value', { humidity: humidity,
-												celsius: celsius,
-												fahrenheit: fahrenheit});
+		humidity_get_val(function(value) {
+			client.emit('humidity ret_value', { humidity: value[0],
+												celsius: value[1],
+												fahrenheit: value[2]});
 		});
 	});
 };
