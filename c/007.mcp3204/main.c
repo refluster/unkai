311d3807125e63a8b8d0366eb5d1e1c8d9d77a9f
refluster <refluster@gmail.com>
2015-05-08 10:39:43 +0900

    IssueID #32 : implement glove moisture sensor driver

--- c/007.mcp3204/main.c
+++ c/007.mcp3204/main.c
@@ -13,12 +13,16 @@ float med3(float *f) {
 
 int main() {
 	float lux[SAMPLE_COUNT];
+	float moisture[SAMPLE_COUNT];
+	float lux_med;
+	float moisture_med;
 	int i;
 	float median;
 
 	// setup wiringpi spi
 	wiringPiSPISetup(0, 1000000) ;
 
+	// njl7502 (lux)
 	for (i = SAMPLE_COUNT - 1; i >= 0; --i) {
 		unsigned char data[] = {0b00000110, 0b00000000, 0b00000000};
 
@@ -29,13 +33,26 @@ int main() {
 		lux[i] = (float)(((data[1] << 8) + data[2]) & 0x0FFF) * 1200.0f / 4096.0f;
 	}
 
+	// GROVE (moisture)
+	for (i = SAMPLE_COUNT - 1; i >= 0; --i) {
+		unsigned char data[] = {0b00000110, 0b01000000, 0b00000000};
+
+		// send and receive data with spi
+		wiringPiSPIDataRW (0, data, sizeof(data));
+	
+		// convert unit to lux
+		moisture[i] = (float)(((data[1] << 8) + data[2]) & 0x0FFF) * 1930.0f / 4096.0f;
+	}
+
 #if SAMPLE_COUNT == 3
-	median = med3(lux);
+	lux_med = med3(lux);
+	moisture_med = med3(moisture);
 #else
 # error "implement a function to find the median"
 #endif	
 
-	printf("illuminance: %.2f lux\n", median);
+	printf("illuminance: %.2f lux\n", lux_med);
+	printf("moisture: %.2f %\n", moisture_med);
 	
 	return 0;
 }
--- nodejs/000.ejs/routes/mcp3204.js
+++ nodejs/000.ejs/routes/mcp3204.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 
 function mcp3204_get_val(callback) {
-	var illuminance;
+	var illuminance, moisture;
 	var cmd = "../../c/007.mcp3204/007.mcp3204";
 	exec(cmd, {timeout: 1000}, function(error, stdout, stderr) {
 		console.log('stdout: '+(stdout||'none'));
@@ -9,13 +9,14 @@ function mcp3204_get_val(callback) {
 		if (error !== null) {
 			console.log('exec error: '+error);
 		} else {
-			var re = /illuminance: (\d+\.\d+) lux/;
+			var re = /illuminance: (\d+\.\d+) lux[\s\S]*moisture: (\d+\.\d+) %/;
 			var re_match = stdout.match(re);
 			if (re_match) {
 				illuminance = re_match[1];
+				moisture = re_match[2];
 			}
 		}
-		callback(illuminance);
+		callback(illuminance, moisture);
 	});
 }
 
@@ -29,13 +30,13 @@ exports.index = function(req, res){
 		ua.indexOf('Android') > 0) {
 		is_mobile = true;
 	}
-	res.render('mcp3204', { title: 'illuminance', host: req.headers.host, is_mobile: is_mobile});
+	res.render('mcp3204', { title: 'mcp3204', host: req.headers.host, is_mobile: is_mobile});
 };
 
 exports.init_socket = function(io, client){
-	client.on('illuminance get_value', function(data) {
-		mcp3204_get_val(function(illuminance) {
-			client.emit('illuminance ret_value', {illuminance: illuminance});
+	client.on('mcp3204 get_value', function(data) {
+		mcp3204_get_val(function(illuminance, moisture) {
+			client.emit('mcp3204 ret_value', {illuminance: illuminance, moisture: moisture});
 		});
 	});
 };
