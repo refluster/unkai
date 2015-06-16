2bfc0057b9eb36a6c257d4778789110dc775544b
refluster <refluster@gmail.com>
2015-06-17 07:06:06 +0900

    IssueID #25 : add moss conditioner lib

---
+++ nodejs/001.ejs/lib/conditioner.js
@@ -0,0 +1,23 @@
+const UPDATE_INTERVAL = 3000; // msec order
+var timer;
+var driver;
+
+function updateCondition() {
+	const MIST_GEN_LOWER_MOISTURE = 120;
+	const MIN_MOISTURE = 0;
+
+	driver.getSensor(function(data) {
+		if (data.moisture < MIST_GEN_LOWER_MOISTURE) {
+			var ratio = (MIST_GEN_LOWER_MOISTURE - data.moisture) /
+				(MIST_GEN_LOWER_MOISTURE - MIN_MOISTURE);
+			driver.setMistgen(Math.floor(ratio * 100));
+		} else {
+			driver.setMistgen(0);
+		}
+	});
+}
+
+exports.init = function(_driver) {
+	driver = _driver;
+	timer = setInterval(updateCondition, UPDATE_INTERVAL);
+};
