201f8de09eb768728dbbaf004ee6da20c080e409
refluster <refluster@gmail.com>
2016-11-09 23:37:47 +0900

    make app.js

--- nodejs/002.christmas/app.js
+++ nodejs/002.christmas/app.js
@@ -8,20 +8,8 @@ var App = function() {
 };
 
 App.prototype.stop = function() {
-	// terminate background processes
-	//   T.B.D.
-
     // close http server
     this.server.close();
-}
-
-App.prototype.isMobile = function(ua) {
-	if ((ua.indexOf('iPhone') > 0 && ua.indexOf('iPad') == -1) ||
-		ua.indexOf('iPod') > 0 ||
-		ua.indexOf('Android') > 0) {
-		return true;
-	}
-	return false;
 }
 
 App.prototype.start = function() {
@@ -40,32 +28,13 @@ App.prototype.start = function() {
 	this.driver = require('./lib/drv');
 	this.driver.start();
 
-	// conditioner
-	this.conditioner = require('./lib/conditioner');
-	this.conditioner.init(this.driver);
-	this.conditioner.start();
-
 	// socket io
 	this.socket = require('./lib/socket');
-	this.socket.init(this.driver, this.server, this.conditioner);
+	this.socket.init(this.driver, this.server);
 
 	// pages
-	this.app.get("/status", function(req, res) {
-		var mobile = false;
-		
-		if (this.isMobile(JSON.stringify(req.headers['user-agent']))) {
-			mobile = true;
-		}
-		res.render('status', {host: req.headers.host, is_mobile: mobile});
-	}.bind(this));
-
 	this.app.get("/", function(req, res) {
-		var mobile = false;
-		
-		if (this.isMobile(JSON.stringify(req.headers['user-agent']))) {
-			mobile = true;
-		}
-		res.render('index', {host: req.headers.host, is_mobile: mobile});
+		res.render('index', {host: req.headers.host});
 	}.bind(this));
 }
 
