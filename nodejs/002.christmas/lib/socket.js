2b1b703aa9b3273934aefc31bc3d0f24e5779c04
refluster <refluster@gmail.com>
2016-11-19 23:53:37 +0900

    add socket api

--- nodejs/002.christmas/lib/socket.js
+++ nodejs/002.christmas/lib/socket.js
@@ -18,6 +18,18 @@ exports.init = function(driver, server) {
 			console.log("led/set " + data.brightness);
 		});
 		
+		// led max brightness
+		client.on('led/setMaxBrightness', function(data) {
+			driver.setLedMaxBrightness(data.brightness);
+			console.log("led/setMaxBrightness " + data.brightness);
+		});
+
+		// led pattern
+		client.on('led/setPattern', function(data) {
+			driver.setLedPattern(data.pattern);
+			console.log("led/setPattern " + data.pattern);
+		});
+
 		// system
 		client.on('system/reset', function() {
 			console.log("system/reset");
