e7faeff82d74a194decee25bef2115e9dd1717c0
refluster <refluster@gmail.com>
2016-11-20 19:53:27 +0900

    update nodejs bin

--- nodejs/002.christmas/start-server.sh
+++ nodejs/002.christmas/start-server.sh
@@ -1,2 +1,2 @@
 cd `dirname $0`
-env NODE_PATH=/usr/lib/node_modules:/usr/local/lib/node_modules node app.js
+env NODE_PATH=/usr/lib/node_modules:/usr/local/lib/node_modules ~/node-v6.9.1-linux-armv6l/bin/node app.js
