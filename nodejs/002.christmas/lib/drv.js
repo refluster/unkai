9196edbfb423460964d72fe2ecfb625f8acd066a
refluster <refluster@gmail.com>
2016-11-20 23:28:27 +0900

    refactor driver

--- nodejs/002.christmas/lib/drv.js
+++ nodejs/002.christmas/lib/drv.js
@@ -1,15 +1,10 @@
 const TEST_DRIVER = 0;
+const num_led = 15; // # of led to control
 
 var spawn = require('child_process').spawn;
-
-// tlc5940 ////////////////////////////
-const num_led = 15; // # of led to control
-var tlc5940_process;
 var tlc5940_brightness = [];
-var brightness_ratio = [];
-var max_brightness = 1000;
 
-function tlc5940_set(brightness) {
+function tlc5940_set(p, brightness) {
 	var cmd = "1 " + brightness.join(" ") + "\n";
 	
 	if (TEST_DRIVER) {
@@ -17,7 +12,7 @@ function tlc5940_set(brightness) {
 		return;
 	}
 	
-	tlc5940_process.stdin.write(cmd);
+	p.stdin.write(cmd);
 	tlc5940_brightness = brightness;
 }
 
@@ -38,7 +33,7 @@ function extend(target) {
 
 // exports ////////////////////////////
 exports.setLed = function(brightness) {
-	tlc5940_set(brightness);
+	tlc5940_set(this.tlc5940_process, brightness);
 };
 
 exports.getLed = function(callback) {
@@ -46,43 +41,46 @@ exports.getLed = function(callback) {
 };
 
 exports.setLedMaxBrightness = function(b) {
-	max_brightness = b;
-	var brightness = brightness_ratio.map(function(r) {return r * max_brightness});
-	tlc5940_set(brightness);
+	this.max_brightness = b;
+	var brightness = this.brightness_ratio.map(function(r) {return r * this.max_brightness}.bind(this));
+	tlc5940_set(this.tlc5940_process, brightness);
 };
 
 exports.setLedPattern = function(b) {
 	if (b == 'on') {
-		for (var i = 0; i < brightness_ratio.length; i++) {
-			brightness_ratio[i] = 1;
+		for (var i = 0; i < this.brightness_ratio.length; i++) {
+			this.brightness_ratio[i] = 1;
 		}
 	} else if (b == 'off') {
-		for (var i = 0; i < brightness_ratio.length; i++) {
-			brightness_ratio[i] = 0;
+		for (var i = 0; i < this.brightness_ratio.length; i++) {
+			this.brightness_ratio[i] = 0;
 		}
 	} else if (b == 'random') {
 	}
-	var brightness = brightness_ratio.map(function(r) {return r * max_brightness});
-	console.log(max_brightness);
-	console.log(brightness_ratio);
-	console.log(brightness);
-	tlc5940_set(brightness);
+	var brightness = this.brightness_ratio.map(function(r) {return r * this.max_brightness}.bind(this));
+	tlc5940_set(this.tlc5940_process, brightness);
 };
 
 exports.start = function() {
+	// tlc5940 ////////////////////////////
+	this.tlc5940_process;
+	this.max_brightness = 1000;
+
 	if (! TEST_DRIVER) {
-		tlc5940_process = spawn("../../c/003.tlc5940/003.tlc5940", ["-n", String(num_led)]);
+		this.tlc5940_process = spawn("../../c/003.tlc5940/003.tlc5940", ["-n", String(num_led)]);
+	}
+
+	this.brightness_ratio = [];
+	for (var i = 0; i < num_led; i++) {
+		this.brightness_ratio.push(1);
 	}
 
 	brightness = [];
 	for (var i = 0; i < num_led; i++) {
 		brightness.push(0);
 	}
+	tlc5940_set(this.tlc5940_process, brightness);
 
-	brightness_ratio = [];
-	for (var i = 0; i < num_led; i++) {
-		brightness_ratio.push(1);
-	}
 
 /*
 	var increment = 100;
@@ -120,5 +118,5 @@ exports.stop = function() {
 		return;
 	}
 	
-	tlc5940_process.stdin.write("0\n");
+	this.tlc5940_process.stdin.write("0\n");
 };
