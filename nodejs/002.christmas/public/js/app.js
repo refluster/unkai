fdd3348eca335e5cd9cbe6d7b90c8f8969fa227a
refluster <refluster@gmail.com>
2016-11-22 23:35:09 +0900

    add app.js for client side

---
+++ nodejs/002.christmas/public/js/app.js
@@ -0,0 +1,58 @@
+var app = angular.module('app', []);
+
+app.controller('cBrightness', function($scope) {
+	var disableUpdate = false;
+
+	$scope.update = function() {
+		if (disableUpdate) {
+			return;
+		}
+		console.log($scope.brightness);
+		socket.emit('led/setMaxBrightness', {brightness: $scope.brightness});
+//		disableUpdate = true;
+		setTimeout(function() {
+			disableUpdate = false;
+		}, 100)
+	};
+});
+
+app.controller('cOptions', function($scope) {
+	$scope.patterns = ['on', 'random', 'off']
+	$scope.pattern = 'on';
+
+	$scope.updatePattern = function() {
+		console.log($scope.pattern);
+		socket.emit('led/setPattern', {pattern: $scope.pattern});
+	};
+});
+
+app.controller('cOn', function($scope) {
+});
+
+app.controller('cRandom', function($scope) {
+	var disableUpdate = false;
+	$scope.updateSpeed = function() {
+		if (disableUpdate) {
+			return;
+		}
+		console.log($scope.speed);
+		socket.emit('led/setRandomInterval', {randomInterval: $scope.speed});
+		disableUpdate = true;
+		setTimeout(function() {
+			disableUpdate = false;
+		}, 100)
+	};
+	$scope.updateNumber = function() {
+		if (disableUpdate) {
+			return;
+		}
+		console.log($scope.number);
+		disableUpdate = true;
+		setTimeout(function() {
+			disableUpdate = false;
+		}, 100)
+	};
+});
+
+app.controller('cOff', function($scope) {
+});
